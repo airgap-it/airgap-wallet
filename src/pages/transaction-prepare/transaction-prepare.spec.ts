edd0928b0d70f73cb26ffb064d56fd99d616844e
Dominik Schöni <dominik@dominikschoeni.ch>
2018-12-10 13:09:46 +0100

    fix(prepare): fixing tx-prepare

--- src/pages/transaction-prepare/transaction-prepare.spec.ts
+++ src/pages/transaction-prepare/transaction-prepare.spec.ts
@@ -7,9 +7,8 @@ import { SplashScreen } from '@ionic-native/splash-screen'
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { Keyboard } from '@ionic-native/keyboard'
-import { BigNumber } from 'bignumber.js'
 
-import { PlatformMock, StatusBarMock, SplashScreenMock, NavParamsMock, NavMock } from '../../../test-config/mocks-ionic'
+import { PlatformMock, StatusBarMock, SplashScreenMock, NavParamsMock } from '../../../test-config/mocks-ionic'
 import { NavControllerMock, KeyboardMock, LoadingControllerMock, ToastControllerMock } from 'ionic-mocks'
 
 import { ComponentsModule } from '../../components/components.module'
@@ -18,8 +17,8 @@ import { WalletMock } from '../../../test-config/wallet-mock'
 import { StorageMock } from '../../../test-config/storage-mock'
 import { Storage } from '@ionic/storage'
 import { PipesModule } from '../../pipes/pipes.module'
-import { TransactionQrPage } from '../transaction-qr/transaction-qr'
-import { Transaction } from '../../models/transaction.model'
+import { Clipboard } from '@ionic-native/clipboard'
+import { ClipboardBrowserProvider } from '../../providers/clipboard-browser/clipboard-browser'
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json')
@@ -66,7 +65,8 @@ describe('TransactionPrepare Page', () => {
         { provide: ToastController, useFactory: () => ToastControllerMock.instance() },
         { provide: SplashScreen, useClass: SplashScreenMock },
         { provide: Platform, useClass: PlatformMock },
-        { provide: Keyboard, useClass: KeyboardMock }
+        { provide: Keyboard, useClass: KeyboardMock },
+        { provide: Clipboard, useClass: ClipboardBrowserProvider }
       ]
     })
   }))
@@ -85,24 +85,34 @@ describe('TransactionPrepare Page', () => {
   it('should select the correct default fee, and low/medium/high fees', done => {
     let el = fixture.debugElement.nativeElement
     let feeAmount = el.querySelector('#fee-amount')
+    let feeAmountAdvanced = el.querySelector('#fee-amount-advanced')
 
     component.useWallet(ethWallet)
-    expect(component.fee).toEqual(ethWallet.coinProtocol.feeDefaults.low.toFixed(-1 * ethWallet.coinProtocol.feeDefaults.low.e + 1))
+    expect(component.transactionForm.value.fee).toEqual(
+      ethWallet.coinProtocol.feeDefaults.low.toFixed(-1 * ethWallet.coinProtocol.feeDefaults.low.e + 1)
+    )
 
     fixture.detectChanges()
 
     // check if fee gets properly displayed in $
-    expect(feeAmount.textContent).toEqual('0.021')
+    expect(feeAmount.textContent).toEqual('$0.021')
+    expect(feeAmountAdvanced.textContent).toEqual('(0.00021 ETH)')
 
     // check if fee changes if set to medium
     component.transactionForm.controls['feeLevel'].setValue(1)
-    expect(component.fee).toEqual(ethWallet.coinProtocol.feeDefaults.medium.toFixed(-1 * ethWallet.coinProtocol.feeDefaults.low.e + 1))
-    expect(feeAmount.textContent).toEqual('0.021')
+    expect(component.transactionForm.value.fee).toEqual(
+      ethWallet.coinProtocol.feeDefaults.medium.toFixed(-1 * ethWallet.coinProtocol.feeDefaults.low.e + 1)
+    )
+    expect(feeAmount.textContent).toEqual('$0.021')
+    expect(feeAmountAdvanced.textContent).toEqual('(0.00021 ETH)')
 
     // check if fee changes if set to high
     component.transactionForm.controls['feeLevel'].setValue(2)
-    expect(component.fee).toEqual(ethWallet.coinProtocol.feeDefaults.high.toFixed(-1 * ethWallet.coinProtocol.feeDefaults.low.e + 1))
-    expect(feeAmount.textContent).toEqual('0.021')
+    expect(component.transactionForm.value.fee).toEqual(
+      ethWallet.coinProtocol.feeDefaults.high.toFixed(-1 * ethWallet.coinProtocol.feeDefaults.low.e + 1)
+    )
+    expect(feeAmount.textContent).toEqual('$0.021')
+    expect(feeAmountAdvanced.textContent).toEqual('(0.00021 ETH)')
 
     done()
   })
@@ -110,42 +120,12 @@ describe('TransactionPrepare Page', () => {
   it('should properly display $ amount of a fee', () => {
     let el = fixture.debugElement.nativeElement
     let feeAmount = el.querySelector('#fee-amount')
-
+    let feeAmountAdvanced = el.querySelector('#fee-amount-advanced')
     component.useWallet(ethWallet)
-    expect(feeAmount.textContent).toEqual('0.021')
+    expect(feeAmount.textContent).toEqual('$0.021')
+    expect(feeAmountAdvanced.textContent).toEqual('(0.00021 ETH)')
   })
 
-  /*
-  it('should create a correct transaction', async () => {
-    expect(component.transactionForm.valid).toBe(false)
-    component.transactionForm.controls['address'].setValue(ethWallet.addresses[0])
-    component.transactionForm.controls['amount'].setValue(0)
-    component.transactionForm.controls['fee'].setValue(0) // set a unrealistic 0 fee to work with mock
-    expect(component.transactionForm.valid).toBe(true)
-
-    await component.prepareTransaction(component.transactionForm.value)
-
-    // should create a loadingCtrl
-    expect((component as any).loadingCtrl.create).toHaveBeenCalled()
-
-    // should push to the next page
-    expect((component as any).navController.push).toHaveBeenCalledWith(TransactionQrPage, {
-      wallet: component.wallet,
-      transaction: new Transaction(
-        [ethWallet.receivingPublicAddress],
-        [ethWallet.receivingPublicAddress],
-        new BigNumber(0),
-        new BigNumber(0),
-        ethWallet.protocolIdentifier
-      ),
-      data:
-        'airgap-vault://sign?data=' +
-        window.btoa(
-          '{"protocolIdentifier":"eth","publicKey":"03ea568e601e6e949a3e5c60e0f4ee94383e4b083c5ab64b66e70372df008cbbe6","payload":{"nonce":0,"gasLimit":21000,"gasPrice":"0","to":"0x4681Df42ca7d5f0E986FFeA979A55c333f5c0a05","from":"0x4681Df42ca7d5f0E986FFeA979A55c333f5c0a05","value":"0","chainId":1}}'
-        )
-    })
-  })
-  */
   it('should create a toast "insufficient balance" if fee + amount is > wallet value', async () => {
     component.transactionForm.controls['address'].setValue(ethWallet.addresses[0])
     component.transactionForm.controls['amount'].setValue(10)
