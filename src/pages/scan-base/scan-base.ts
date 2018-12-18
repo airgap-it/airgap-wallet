16cad607bf6488eeeb10647748b31290074b782e
Andreas Gassmann <andreas@andreasgassmann.ch>
2018-12-18 16:15:47 +0100

    refactor(scan): remove duplicate code

---
+++ src/pages/scan-base/scan-base.ts
@@ -0,0 +1,86 @@
+import { Platform } from 'ionic-angular'
+import { ScannerProvider } from '../../providers/scanner/scanner'
+import { PermissionsProvider, PermissionTypes, PermissionStatus } from '../../providers/permissions/permissions'
+import { ZXingScannerComponent } from '@zxing/ngx-scanner'
+
+export class ScanBasePage {
+  zxingScanner: ZXingScannerComponent
+  availableDevices: MediaDeviceInfo[]
+  selectedDevice: MediaDeviceInfo
+  scannerEnabled = true
+
+  public isBrowser = false
+
+  hasCameras = false
+
+  public hasCameraPermission = false
+
+  constructor(protected platform: Platform, protected scanner: ScannerProvider, protected permissionsProvider: PermissionsProvider) {}
+
+  async ionViewWillEnter() {
+    if (this.platform.is('cordova')) {
+      await this.platform.ready()
+      await this.checkCameraPermissionsAndActivate()
+    }
+  }
+
+  async requestPermission() {
+    await this.permissionsProvider.userRequestsPermissions([PermissionTypes.CAMERA])
+    await this.checkCameraPermissionsAndActivate()
+  }
+
+  async checkCameraPermissionsAndActivate() {
+    const permission = await this.permissionsProvider.hasCameraPermission()
+    if (permission === PermissionStatus.GRANTED) {
+      this.hasCameraPermission = true
+      this.startScan()
+    }
+  }
+
+  ionViewDidEnter() {
+    if (!this.platform.is('cordova')) {
+      this.hasCameraPermission = true
+      this.zxingScanner.camerasNotFound.subscribe((devices: MediaDeviceInfo[]) => {
+        console.error('An error has occurred when trying to enumerate your video-stream-enabled devices.')
+      })
+      if (this.selectedDevice) {
+        // Not the first time that we open scanner
+        this.zxingScanner.startScan(this.selectedDevice)
+      }
+      this.zxingScanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+        this.hasCameras = true
+        this.availableDevices = devices
+        this.selectedDevice = devices[0]
+      })
+    }
+  }
+
+  ionViewWillLeave() {
+    if (this.platform.is('cordova')) {
+      this.scanner.destroy()
+    } else {
+      this.zxingScanner.resetCodeReader()
+    }
+  }
+
+  public startScan() {
+    if (this.platform.is('cordova')) {
+      this.scanner.show()
+      this.scanner.scan(
+        text => {
+          this.checkScan(text)
+        },
+        error => {
+          console.warn(error)
+          this.startScan()
+        }
+      )
+    } else {
+      // We don't need to do anything in the browser because it keeps scanning
+    }
+  }
+
+  checkScan(resultString: string) {
+    console.error(`The checkScan method needs to be overwritten. Ignoring text ${resultString}`)
+  }
+}
