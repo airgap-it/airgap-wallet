d100316296e2e810295469049b58980f8682e06f
Pascal Brun <brunpasc@gmail.com>
2018-12-20 23:18:12 +0100

    feat(exchange): removes modules file

--- src/pages/exchange/exchange.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core'
-import { IonicPageModule } from 'ionic-angular'
-import { ExchangePage } from './exchange'
-
-@NgModule({
-  declarations: [ExchangePage],
-  imports: [IonicPageModule.forChild(ExchangePage)]
-})
-export class ExchangePageModule {}
