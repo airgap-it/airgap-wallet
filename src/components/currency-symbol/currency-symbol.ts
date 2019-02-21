8d8dc93bd48b7e481ca1e29496d164abacd8cbe0
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-02-21 18:56:14 +0100

    fix(image): symbol placeholder was loaded by accident

--- src/components/currency-symbol/currency-symbol.ts
+++ src/components/currency-symbol/currency-symbol.ts
@@ -8,17 +8,18 @@ export class CurrencySymbolComponent {
   @Input()
   private symbol: string
 
-  public symbolURL: string = ''
+  public symbolURL: string = 'assets/symbols/btc.svg' // TODO: Add fallback image
 
   constructor() {
     /* */
   }
 
   ngAfterViewInit() {
-    this.symbolURL = 'assets/symbols/' + this.symbol.toLowerCase() + '.svg'
-  }
-
-  errorHandler() {
-    this.symbolURL = 'assets/symbols/btc.svg' // TODO: fallback image
+    const imageUrl = 'assets/symbols/' + this.symbol.toLowerCase() + '.svg'
+    const img = new Image()
+    img.onload = () => {
+      this.symbolURL = imageUrl
+    }
+    img.src = imageUrl
   }
 }
