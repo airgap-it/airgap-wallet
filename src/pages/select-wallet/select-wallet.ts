ce80cd1615891c2664f85fd7afec2ba4036278c5
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-03-12 13:22:50 +0100

    chore(select): rename variables

--- src/pages/select-wallet/select-wallet.ts
+++ src/pages/select-wallet/select-wallet.ts
@@ -1,6 +1,6 @@
-import { ErrorCategory, handleErrorSentry } from './../../providers/sentry-error-handler/sentry-error-handler'
+import { ErrorCategory, handleErrorSentry } from '../../providers/sentry-error-handler/sentry-error-handler'
 import { AirGapMarketWallet } from 'airgap-coin-lib'
-import { AccountProvider } from './../../providers/account/account.provider'
+import { AccountProvider } from '../../providers/account/account.provider'
 import { Component } from '@angular/core'
 import { NavController, NavParams } from 'ionic-angular'
 import { TransactionPreparePage } from '../transaction-prepare/transaction-prepare'
@@ -10,14 +10,16 @@ import { TransactionPreparePage } from '../transaction-prepare/transaction-prepa
   templateUrl: 'select-wallet.html'
 })
 export class SelectWalletPage {
-  address: string
-  wallets: AirGapMarketWallet[] = []
-  incompatibleWallets: AirGapMarketWallet[] = []
+  public compatibleWallets: AirGapMarketWallet[] = []
+  public incompatibleWallets: AirGapMarketWallet[] = []
+
+  private address: string
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public accountProvider: AccountProvider) {}
 
   ionViewDidLoad() {
     this.address = this.navParams.get('address')
-    this.wallets = this.accountProvider.getCompatibleWallets()
+    this.compatibleWallets = this.accountProvider.getCompatibleWallets()
     this.incompatibleWallets = this.accountProvider.getIncompatibleWallets()
   }
 
