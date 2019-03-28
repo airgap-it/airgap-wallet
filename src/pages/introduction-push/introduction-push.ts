0fd677724854faac1f2ee6f4dda08e44bbae7e53
Pascal Brun <brunpasc@gmail.com>
2019-03-28 18:12:30 +0100

    feat(introduction-push): initial styling

---
+++ src/pages/introduction-push/introduction-push.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { IonicPage, NavController, NavParams } from 'ionic-angular'
+
+@IonicPage()
+@Component({
+  selector: 'page-introduction-push',
+  templateUrl: 'introduction-push.html'
+})
+export class IntroductionPushPage {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+}
