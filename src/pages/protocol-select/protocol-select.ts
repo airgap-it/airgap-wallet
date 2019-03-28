a224fc3f4637721e4aa29bead742b5e9d243bc0a
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-03-28 16:18:18 +0100

    feat(exchange): add currency selection modal

--- src/pages/protocol-select/protocol-select.ts
+++ src/pages/protocol-select/protocol-select.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-import { NavController, NavParams } from 'ionic-angular'
+import { NavController, NavParams, ViewController } from 'ionic-angular'
 import { ICoinProtocol } from 'airgap-coin-lib'
 
 @Component({
@@ -7,11 +7,36 @@ import { ICoinProtocol } from 'airgap-coin-lib'
   templateUrl: 'protocol-select.html'
 })
 export class ProtocolSelectPage {
+  searchTerm: string = ''
+
   selectedProtocol: ICoinProtocol
   protocols: ICoinProtocol[]
+  filteredProtocols: ICoinProtocol[]
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
     this.selectedProtocol = this.navParams.get('selectedProtocol')
     this.protocols = this.navParams.get('protocols')
+    this.searchTermChanged()
+  }
+
+  public dismiss() {
+    this.viewCtrl.dismiss()
+  }
+
+  public select() {
+    console.log('protocol', this.selectedProtocol)
+    this.viewCtrl.dismiss(this.selectedProtocol)
+  }
+
+  public searchTermChanged() {
+    console.log('search term changed', this.searchTerm)
+    this.filteredProtocols = this.filterProtocols()
+  }
+
+  private filterProtocols() {
+    const lowerCaseSearchTerm = this.searchTerm.toLowerCase()
+    return this.protocols.filter(
+      protocol => protocol.name.toLowerCase().includes(lowerCaseSearchTerm) || protocol.symbol.toLowerCase().includes(lowerCaseSearchTerm)
+    )
   }
 }
