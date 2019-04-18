9469b88a0cf150491acb4214dc8777a9dcec5eed
acharl <amadeo@fermat.org>
2019-04-18 17:56:40 +0200

    feat(permission-popup): opens popup which asks for confirmation to share wallet

--- src/pages/extension-share-permission/extension-share-permission.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core'
-import { IonicPageModule } from 'ionic-angular'
-import { ExtensionSharePermissionPage } from './extension-share-permission'
-
-@NgModule({
-  declarations: [ExtensionSharePermissionPage],
-  imports: [IonicPageModule.forChild(ExtensionSharePermissionPage)]
-})
-export class ExtensionSharePermissionPageModule {}
