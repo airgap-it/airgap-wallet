d326187a9f77884840b5959cc319b94302aec580
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-05-04 18:05:13 +0200

    feat(ae): show last vote and get config from backend

--- src/pages/voting/voting.ts
+++ src/pages/voting/voting.ts
@@ -1,23 +1,62 @@
 import { Component } from '@angular/core'
-import { NavController, NavParams } from 'ionic-angular'
+import { NavController, NavParams, Platform } from 'ionic-angular'
 import BigNumber from 'bignumber.js'
 import { InteractionSelectionPage } from '../interaction-selection/interaction-selection'
-import { AirGapMarketWallet } from 'airgap-coin-lib'
+import { AirGapMarketWallet, IAirGapTransaction } from 'airgap-coin-lib'
 import { handleErrorSentry, ErrorCategory } from '../../providers/sentry-error-handler/sentry-error-handler'
 import { OperationsProvider } from '../../providers/operations/operations'
+import { RemoteConfigProvider, AeFirstVote } from '../../providers/remote-config/remote-config'
+
+declare let cordova
 
 @Component({
   selector: 'page-voting',
   templateUrl: 'voting.html'
 })
 export class VotingPage {
+  public oldVotePercentage: number
+  public config: AeFirstVote
   public wallet: AirGapMarketWallet
 
-  public votePercentage: number = 0
+  public votePercentage: number = 10
   public votingAddress: string = 'ak_11111111111111111111111111111111273Yts'
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private operationsProvider: OperationsProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private operationsProvider: OperationsProvider,
+    private remoteConfigProvider: RemoteConfigProvider,
+    private platform: Platform
+  ) {
     this.wallet = this.navParams.get('wallet')
+  }
+
+  ngOnInit() {
+    this.remoteConfigProvider.aeFirstVote().then(config => (this.config = config))
+
+    this.wallet
+      .fetchTransactions(1000, 0)
+      .then(txs => {
+        const oldVote = txs.find(tx => !!tx.data)
+        console.log(oldVote)
+        if (oldVote) {
+          try {
+            console.log(JSON.parse(oldVote.data))
+            this.oldVotePercentage = JSON.parse(oldVote.data).vote.option
+          } catch (error) {
+            console.log(error)
+          }
+        }
+      })
+      .catch(console.log)
+  }
+
+  public openUrl(url: string) {
+    if (this.platform.is('ios') || this.platform.is('android')) {
+      cordova.InAppBrowser.open(url, '_system', 'location=true')
+    } else {
+      window.open(url, '_blank')
+    }
   }
 
   public async vote() {
