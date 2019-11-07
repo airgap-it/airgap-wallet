3458fbb6d0b8bb2e56a5599e706b1494014da6ab
acharl <amadeo@fermat.org>
2019-11-07 14:01:31 +0100

    feat(app): fetches all validators as well as your validators

--- src/app/pages/delegation-validator-list/delegation-validator-list.page.ts
+++ src/app/pages/delegation-validator-list/delegation-validator-list.page.ts
@@ -1,12 +1,35 @@
+import { ValidatorService } from './../../services/validator/validator.service'
 import { Component, OnInit } from '@angular/core'
+import { CosmosValidator, CosmosDelegation } from 'airgap-coin-lib/dist/protocols/cosmos/CosmosNodeClient'
+import { ActivatedRoute } from '@angular/router'
+import { AirGapMarketWallet } from 'airgap-coin-lib'
 
 @Component({
   selector: 'delegation-validator-list',
   templateUrl: './delegation-validator-list.page.html',
   styleUrls: ['./delegation-validator-list.page.scss']
 })
-export class DelegationValidatorListPage implements OnInit {
-  constructor() {}
+export class DelegationValidatorListPage {
+  public validators: CosmosValidator[]
+  public delegations: CosmosDelegation[]
 
-  ngOnInit() {}
+  private wallet: AirGapMarketWallet
+  constructor(private readonly validatorService: ValidatorService, private readonly route: ActivatedRoute) {
+    if (this.route.snapshot.data.special) {
+      const info = this.route.snapshot.data.special
+      this.wallet = info.wallet
+      this.fetchDelegations(this.wallet.addresses[0])
+      this.fetchValidatorList()
+    }
+  }
+
+  private async fetchValidatorList() {
+    this.validators = await this.validatorService.fetchValidators()
+  }
+
+  private async fetchDelegations(address: string) {
+    const delegations = await this.validatorService.fetchDelegations(address)
+    const validatorPromise = delegations.map(delegation => this.validatorService.fetchValidator(delegation.validator_address))
+    this.validators = await Promise.all(validatorPromise)
+  }
 }
--- src/app/services/validator/validator.service.ts
+++ src/app/services/validator/validator.service.ts
@@ -1,7 +1,7 @@
 import { CosmosProtocol } from 'airgap-coin-lib'
 import { BigNumber } from 'bignumber.js'
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+import { CosmosValidator, CosmosDelegation } from 'airgap-coin-lib/dist/protocols/cosmos/CosmosNodeClient'
 
 export interface Uptime {
   address: string
@@ -20,16 +20,17 @@ export interface CosmosValidatorInfo {
   providedIn: 'root'
 })
 export class ValidatorService {
+  public protocol = new CosmosProtocol()
+
   public async getValidatorInfo(address: string): Promise<CosmosValidatorInfo> {
     const statusCodes = { 0: 'jailed', 1: 'inactive', 2: 'active' }
-    const protocol = new CosmosProtocol()
     try {
-      const validator = await protocol.fetchValidator(address)
+      const validator = await this.protocol.fetchValidator(address)
       return {
         alias: validator.description.moniker,
         rate: `${(parseFloat(validator.commission.rate) * 100).toString()}%`,
         status: statusCodes[validator.status],
-        totalDelegationBalance: `${new BigNumber(validator.tokens).shiftedBy(-1 * protocol.decimals).toString()}` // TODO display in a nice format
+        totalDelegationBalance: `${new BigNumber(validator.tokens).shiftedBy(-1 * this.protocol.decimals).toString()}` // TODO display in a nice format
       }
     } catch {
       return {
@@ -39,5 +40,17 @@ export class ValidatorService {
         totalDelegationBalance: 'unknown'
       }
     }
+  }
+
+  public async fetchValidators(): Promise<CosmosValidator[]> {
+    return this.protocol.fetchValidators()
+  }
+
+  public async fetchDelegations(address: string): Promise<CosmosDelegation[]> {
+    return this.protocol.fetchDelegations(address)
+  }
+
+  public async fetchValidator(address: string): Promise<CosmosValidator> {
+    return this.protocol.fetchValidator(address)
   }
 }
