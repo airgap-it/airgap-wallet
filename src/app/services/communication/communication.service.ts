d267066aaba8579c3ab51fe316de828560d27269
acharl <amadeo@fermat.org>
2019-12-20 18:22:54 +0100

    feat(beacon): listens for permisssion request, displays permission popup

--- src/app/services/communication/communication.service.ts
+++ src/app/services/communication/communication.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core'
-
 import { SettingsKey, StorageProvider } from '../storage/storage'
-
-import { WalletCommunicationClient } from './WalletCommunicationClient'
+import { WalletCommunicationClient } from '@airgap/beacon-sdk'
+import { Serializer } from '@airgap/beacon-sdk/dist/client/Serializer'
+import { MessageTypes, PermissionRequest } from '@airgap/beacon-sdk/dist/client/Messages'
+import { AlertController } from '@ionic/angular'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommunicationService {
   private client: WalletCommunicationClient | undefined
-  constructor(private readonly storageService: StorageProvider) {
+  constructor(private readonly storageService: StorageProvider, private readonly alertController: AlertController) {
     this.init()
   }
 
@@ -34,6 +35,86 @@ export class CommunicationService {
 
     this.client.listenForEncryptedMessage(pubKey, message => {
       console.log('WALLET gotEncryptedMessage:', message)
+
+      console.log('typeof', typeof message)
+      try {
+        const serializer = new Serializer()
+        const deserializedMessage = serializer.deserialize(message.toString()) as PermissionRequest
+        console.log('deserializedMessage.id', deserializedMessage.id)
+        if (deserializedMessage.type === MessageTypes.PermissionRequest) {
+          this.alertController
+            .create({
+              header: 'Permission request',
+              message: 'Do you want to give the dapp permissions to do all the things?',
+              inputs: [
+                {
+                  name: 'read_address',
+                  type: 'checkbox',
+                  label: 'Read Address',
+                  value: 'read_address',
+                  checked: true
+                },
+
+                {
+                  name: 'sign',
+                  type: 'checkbox',
+                  label: 'Sign',
+                  value: 'sign',
+                  checked: true
+                },
+
+                {
+                  name: 'payment_request',
+                  type: 'checkbox',
+                  label: 'Payment request',
+                  value: 'payment_request',
+                  checked: true
+                },
+
+                {
+                  name: 'threshold',
+                  type: 'checkbox',
+                  label: 'Threshold',
+                  value: 'threshold',
+                  checked: true
+                }
+              ],
+              buttons: [
+                {
+                  text: 'Ok',
+                  handler: () => {
+                    console.log('SATISFIED')
+                    const permissionResponse = {
+                      id: 'string',
+                      type: MessageTypes.PermissionResponse,
+                      address: 'string',
+                      networks: ['1', '2'],
+                      permissions: ['PermissionScope[]']
+                    }
+                    const serializedMessage = serializer.serialize(permissionResponse)
+                    this.client.sendMessage(pubKey, serializedMessage)
+                    console.log('WALLET ALLOWED PERMISSION')
+                  }
+                },
+                {
+                  text: 'Cancel',
+                  role: 'cancel'
+                }
+              ]
+            })
+            .then(alert => {
+              alert.present()
+            })
+        }
+      } catch (error) {}
     })
+  }
+
+  public async sendMessage(pubKey: string, message: string) {
+    if (!this.client) {
+      throw new Error('Client not ready')
+    }
+    console.log('opening channel')
+    this.client.sendMessage(pubKey, message) // TODO: Should we have a confirmation here?
   }
 }
