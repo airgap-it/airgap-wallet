99184dc05e74e21df329550202bbb416aaf50ca7
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-01-10 08:12:47 +0000

    fix(test): use unithelper

--- src/app/services/communication/communication.service.spec.ts
+++ src/app/services/communication/communication.service.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from '@angular/core/testing'
 
+import { UnitHelper } from '../../../../test-config/unit-test-helper'
+
 import { CommunicationService } from './communication.service'
 
 describe('CommunicationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}))
+  let unitHelper: UnitHelper
+  beforeEach(() => {
+    unitHelper = new UnitHelper()
+
+    TestBed.configureTestingModule(
+      unitHelper.testBed({
+        providers: []
+      })
+    )
+      .compileComponents()
+      .catch(console.error)
+  })
 
   it('should be created', () => {
     const service: CommunicationService = TestBed.get(CommunicationService)
