e3c986e6cddea83f60ef3c50346e8071428da764
acharl <amadeo@fermat.org>
2020-02-13 18:06:20 +0100

    feat(swap): manually calculate and add fee for ChangeNow

--- src/app/pages/exchange-confirm/exchange-confirm.ts
+++ src/app/pages/exchange-confirm/exchange-confirm.ts
@@ -49,19 +49,25 @@ export class ExchangeConfirmPage {
       this.amountExpectedFrom = this.exchangeResult.amountExpectedFrom ? this.exchangeResult.amountExpectedFrom : info.amountExpectedFrom
       this.amountExpectedTo = this.exchangeResult.amountExpectedTo ? this.exchangeResult.amountExpectedTo : info.amountExpectedTo
     }
+    const txFee = this.fromWallet.coinProtocol.feeDefaults.medium
 
     const fromAmount = new BigNumber(this.amountExpectedFrom)
-
     const changellyFee = this.exchangeResult.changellyFee ? new BigNumber(this.exchangeResult.changellyFee) : new BigNumber(0)
     const apiExtraFee = this.exchangeResult.apiExtraFee ? new BigNumber(this.exchangeResult.apiExtraFee) : new BigNumber(0)
-    const totalFeeInPercent = changellyFee.plus(apiExtraFee)
-    const txFee = this.fromWallet.coinProtocol.feeDefaults.medium
-    const exchangeTotalFee = fromAmount.multipliedBy(totalFeeInPercent.dividedBy(100))
-    this.fee = exchangeTotalFee.plus(txFee)
-
     this.fromFiatAmount = new BigNumber(this.amountExpectedFrom).multipliedBy(this.fromWallet.currentMarketPrice).toNumber()
-    this.feeFiatAmount = this.fee.multipliedBy(this.fromWallet.currentMarketPrice).toNumber()
     this.toFiatAmount = new BigNumber(this.amountExpectedTo).multipliedBy(this.toWallet.currentMarketPrice).toNumber()
+
+    if (changellyFee.eq(new BigNumber(0)) && apiExtraFee.eq(new BigNumber(0))) {
+      const changeNowFiatFee = new BigNumber(this.fromFiatAmount).minus(new BigNumber(this.toFiatAmount)) // this is the fiat Value which has to be converted to the respective from unit
+      const marketPrice = new BigNumber(this.fromWallet.currentMarketPrice)
+      BigNumber.set({ DECIMAL_PLACES: 7 })
+      this.fee = changeNowFiatFee.div(marketPrice)
+    } else {
+      const totalFeeInPercent = changellyFee.plus(apiExtraFee)
+      const exchangeTotalFee = fromAmount.multipliedBy(totalFeeInPercent.dividedBy(100))
+      this.fee = exchangeTotalFee.plus(txFee)
+    }
+    this.feeFiatAmount = this.fee.multipliedBy(this.fromWallet.currentMarketPrice).toNumber()
   }
 
   public async prepareTransaction() {
