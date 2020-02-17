3d3f437a6034e9d448845228b90b04e6dd7c8e67
acharl <amadeo@fermat.org>
2020-02-17 10:19:22 +0100

    feat(swap): display only tx fee instead of including exchange fee

--- src/app/pages/exchange-confirm/exchange-confirm.ts
+++ src/app/pages/exchange-confirm/exchange-confirm.ts
@@ -16,7 +16,9 @@ declare let cordova
 export class ExchangeConfirmPage {
   public fromWallet: AirGapMarketWallet
   public toWallet: AirGapMarketWallet
-  public fee: BigNumber
+
+  public fee: string
+  public feeFiatAmount: string
 
   public amountExpectedFrom: number
   public amountExpectedTo: number
@@ -25,7 +27,6 @@ export class ExchangeConfirmPage {
   public toCurrency: string
 
   public fromFiatAmount: number
-  public feeFiatAmount: number
   public toFiatAmount: number
 
   public exchangeResult: any // | CreateTransactionResponse
@@ -49,25 +50,10 @@ export class ExchangeConfirmPage {
       this.amountExpectedFrom = this.exchangeResult.amountExpectedFrom ? this.exchangeResult.amountExpectedFrom : info.amountExpectedFrom
       this.amountExpectedTo = this.exchangeResult.amountExpectedTo ? this.exchangeResult.amountExpectedTo : info.amountExpectedTo
     }
-    const txFee = this.fromWallet.coinProtocol.feeDefaults.medium
-
-    const fromAmount = new BigNumber(this.amountExpectedFrom)
-    const changellyFee = this.exchangeResult.changellyFee ? new BigNumber(this.exchangeResult.changellyFee) : new BigNumber(0)
-    const apiExtraFee = this.exchangeResult.apiExtraFee ? new BigNumber(this.exchangeResult.apiExtraFee) : new BigNumber(0)
+    this.fee = this.fromWallet.coinProtocol.feeDefaults.medium
+    this.feeFiatAmount = new BigNumber(this.fee).multipliedBy(this.fromWallet.currentMarketPrice).toString()
     this.fromFiatAmount = new BigNumber(this.amountExpectedFrom).multipliedBy(this.fromWallet.currentMarketPrice).toNumber()
     this.toFiatAmount = new BigNumber(this.amountExpectedTo).multipliedBy(this.toWallet.currentMarketPrice).toNumber()
-
-    if (changellyFee.eq(new BigNumber(0)) && apiExtraFee.eq(new BigNumber(0))) {
-      const changeNowFiatFee = new BigNumber(this.fromFiatAmount).minus(new BigNumber(this.toFiatAmount)) // this is the fiat Value which has to be converted to the respective from unit
-      const marketPrice = new BigNumber(this.fromWallet.currentMarketPrice)
-      BigNumber.set({ DECIMAL_PLACES: 7 })
-      this.fee = changeNowFiatFee.div(marketPrice)
-    } else {
-      const totalFeeInPercent = changellyFee.plus(apiExtraFee)
-      const exchangeTotalFee = fromAmount.multipliedBy(totalFeeInPercent.dividedBy(100))
-      this.fee = exchangeTotalFee.plus(txFee)
-    }
-    this.feeFiatAmount = this.fee.multipliedBy(this.fromWallet.currentMarketPrice).toNumber()
   }
 
   public async prepareTransaction() {
