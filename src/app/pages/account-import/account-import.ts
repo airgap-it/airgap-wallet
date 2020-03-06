6365e4e689ad8b0aca18d0c77bc81625e4fafee0
Julia Samól <j.samol@papers.ch>
2020-03-06 16:56:55 +0100

    fix(ionic): prevent navigating back to account-import

--- src/app/pages/account-import/account-import.ts
+++ src/app/pages/account-import/account-import.ts
@@ -109,6 +109,6 @@ export class AccountImportPage {
 
   public async import(): Promise<void> {
     await this.wallets.addWallet(this.wallet)
-    await this.router.navigateByUrl('/tabs/portfolio')
+    await this.router.navigateByUrl('/tabs/portfolio', { skipLocationChange: true })
   }
 }
