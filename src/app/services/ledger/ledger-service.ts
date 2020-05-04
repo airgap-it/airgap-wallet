893adea58c0210327bfd4e151052b4cb0066d4e5
Julia Samól <j.samol@papers.ch>
2020-05-04 10:04:52 +0200

    feat(ledger): add supported ledger apps

--- src/app/services/ledger/ledger-service.ts
+++ src/app/services/ledger/ledger-service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core'
 
 import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
 import BluetoothTransport from '@ledgerhq/hw-transport-node-ble'
+import { ProtocolSymbols } from '../protocols/protocols'
+import Transport from '@ledgerhq/hw-transport'
+import { LedgerApp } from 'src/app/ledger-apps/LedgerApp'
+import { KusamaLedgerApp } from 'src/app/ledger-apps/substrate/KusamaLedgerApp'
+import { PolkadotLedgerApp } from 'src/app/ledger-apps/substrate/PolkadotLedgerApp'
+import { AirGapMarketWallet } from 'airgap-coin-lib'
 
 export enum LedgerConnectionType {
   USB,
@@ -17,10 +23,23 @@ export interface LedgerConnection {
   providedIn: 'root'
 })
 export class LedgerService {
+  public readonly supportedApps: Map<ProtocolSymbols, (transport: Transport) => LedgerApp> = new Map([
+    [ProtocolSymbols.KUSAMA, (transport: Transport) => new KusamaLedgerApp(transport)],
+    [ProtocolSymbols.POLKADOT, (transport: Transport) => new PolkadotLedgerApp(transport)]
+  ] as [ProtocolSymbols, (transport: Transport) => LedgerApp][])
+
   public async getConnectedDevices(): Promise<LedgerConnection[]> {
     const devices = await Promise.all([this.getUsbDevices(), this.getBleDevices()])
 
     return devices.reduce((flatten, toFlatten) => flatten.concat(toFlatten), [])
+  }
+
+  public async importWallet(identifier: ProtocolSymbols, ledgerConnection: LedgerConnection): Promise<AirGapMarketWallet> {
+    return this.openApp(identifier, ledgerConnection, app => app.importWallet())
+  }
+
+  public async signTransaction(identifier: ProtocolSymbols, ledgerConnection: LedgerConnection, transaction: any): Promise<string> {
+    return this.openApp(identifier, ledgerConnection, app => app.signTranscation(transaction))
   }
 
   private async getUsbDevices(): Promise<LedgerConnection[]> {
@@ -50,5 +69,31 @@ export class LedgerService {
         complete: () => resolve(devices)
       })
     })
+  }
+
+  private async openApp<T>(
+    identifier: ProtocolSymbols,
+    ledgerConnection: LedgerConnection,
+    action: (app: LedgerApp) => Promise<T>
+  ): Promise<T> {
+    const appFactory: (transport: Transport) => LedgerApp = this.supportedApps[identifier]
+
+    if (appFactory) {
+      const transport = await this.connectWithDevice(ledgerConnection)
+      const app = appFactory(transport)
+
+      return action(app).finally(() => transport.close())
+    } else {
+      return Promise.reject('Protocol app is not supported')
+    }
+  }
+
+  private async connectWithDevice(ledgerConnection: LedgerConnection): Promise<Transport> {
+    switch (ledgerConnection.connectionType) {
+      case LedgerConnectionType.USB:
+        return TransportNodeHid.open(ledgerConnection.id)
+      case LedgerConnectionType.BLE:
+        return BluetoothTransport.open(ledgerConnection.id)
+    }
   }
 }
