2336513c93bd2b37cb916e4cea32d8cadda23d0a
Julia Samól <j.samol@papers.ch>
2020-06-02 17:02:12 +0200

    feat(delegation): add pagination in delegation list

--- src/app/pages/delegation-list/delegation-list.page.ts
+++ src/app/pages/delegation-list/delegation-list.page.ts
@@ -22,44 +22,60 @@ export class DelegationListPage {
   public knownDelegatees: UIAccountSummary[]
   public filteredDelegatees: UIAccountSummary[]
 
-  private readonly callback: (address: string) => void
+  private callback: (address: string) => void
 
   constructor(
     private readonly route: ActivatedRoute,
     private readonly navController: NavController,
     private readonly operations: OperationsProvider
-  ) {
+  ) {}
+
+  ngOnInit() {
     if (this.route.snapshot.data.special) {
       const info = this.route.snapshot.data.special
       this.wallet = info.wallet
       this.delegateeLabelPlural = info.delegateeLabelPlural
       this.callback = info.callback
 
-      this.operations.getDelegateesSummary(this.wallet, info.currentDelegatees).then(summary => {
+      this.operations.getDelegateesSummary(this.wallet, info.currentDelegatees).then((summary: UIAccountSummary[]) => {
         this.currentDelegatees = summary.filter(summary => info.currentDelegatees.includes(summary.address))
         this.knownDelegatees = summary.filter(summary => !info.currentDelegatees.includes(summary.address))
 
-        this.filteredDelegatees = this.knownDelegatees
+        this.filteredDelegatees = this.getKnownDelegatees()
       })
     }
   }
 
-  public setFilteredItems(searchTerm: string) {
+  public setFilteredItems(searchTerm: string): void {
     if (searchTerm.length === 0) {
-      this.filteredDelegatees = this.knownDelegatees
+      this.filteredDelegatees = this.getKnownDelegatees()
     } else {
-      this.filteredDelegatees = this.knownDelegatees.filter(delegatee => {
-        const searchTermLowerCase = searchTerm.toLowerCase()
-        const hasMatchingAddress = delegatee.address.toLowerCase().includes(searchTermLowerCase)
-        const hasMatchingName = delegatee.header[0].toLowerCase().includes(searchTermLowerCase)
+      this.filteredDelegatees = this.knownDelegatees.filter((delegatee: UIAccountSummary) => {
+        const searchTermLowerCase: string = searchTerm.toLowerCase()
+        const hasMatchingAddress: boolean = delegatee.address.toLowerCase().includes(searchTermLowerCase)
+        const hasMatchingName: boolean = delegatee.header[0].toLowerCase().includes(searchTermLowerCase)
 
         return hasMatchingAddress || hasMatchingName
       })
     }
   }
 
-  public navigateToDetails(address: string) {
+  public loadMoreItems(event: any): void {
+    this.filteredDelegatees = [...this.filteredDelegatees, ...this.getKnownDelegatees(this.filteredDelegatees.length - 1)].filter(
+      (value: UIAccountSummary, index: number, array: UIAccountSummary[]) => array.indexOf(value) === index
+    )
+    event.target.complete()
+    if (this.filteredDelegatees.length === this.knownDelegatees.length) {
+      event.target.disable = true
+    }
+  }
+
+  public navigateToDetails(address: string): void {
     this.callback(address)
     this.navController.pop()
+  }
+
+  private getKnownDelegatees(startIndex: number = 0, step: number = 10): UIAccountSummary[] {
+    return this.knownDelegatees.slice(0, startIndex + step)
   }
 }
