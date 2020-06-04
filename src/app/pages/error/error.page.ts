67e4752f6a80eaf0478bf6331fa4d3ed9ed73d34
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-06-04 15:34:17 +0000

    feat(beacon): format error

--- src/app/pages/error/error.page.ts
+++ src/app/pages/error/error.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, AfterViewInit } from '@angular/core'
 import { ModalController } from '@ionic/angular'
 
 @Component({
@@ -6,12 +6,17 @@ import { ModalController } from '@ionic/angular'
   templateUrl: './error.page.html',
   styleUrls: ['./error.page.scss']
 })
-export class ErrorPage {
+export class ErrorPage implements AfterViewInit {
   public title: string | undefined
   public message: string | undefined
   public data: unknown
+  public parsedData: string
 
   constructor(private readonly modalController: ModalController) {}
+
+  ngAfterViewInit() {
+    this.parsedData = typeof this.data === 'object' ? JSON.stringify(this.data, null, 2) : this.data + ''
+  }
 
   public async dismiss(): Promise<void> {
     await this.modalController.dismiss(true)
