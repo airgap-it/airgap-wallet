54e9bda2210cbd12e2ccf78fe60feca6ab94a8d6
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-06-30 14:38:23 +0000

    fix(test): add unit helper

--- src/app/services/price/price.service.spec.ts
+++ src/app/services/price/price.service.spec.ts
@@ -1,9 +1,18 @@
 import { TestBed } from '@angular/core/testing'
 
+import { UnitHelper } from '../../../../test-config/unit-test-helper'
+
 import { PriceService } from './price.service'
 
 describe('PriceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}))
+  let unitHelper: UnitHelper
+  beforeEach(() => {
+    unitHelper = new UnitHelper()
+
+    TestBed.configureTestingModule(unitHelper.testBed({}))
+      .compileComponents()
+      .catch(console.error)
+  })
 
   it('should be created', () => {
     const service: PriceService = TestBed.get(PriceService)
