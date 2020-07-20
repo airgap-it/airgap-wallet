10dc1b19c3303b7c12fb882c1bcb7834ea5f9ade
AndreasGassmann <andreas@andreasgassmann.ch>
2020-07-20 21:53:42 +0000

    fix(test): fix empty tests

--- src/app/services/walletconnect/walletconnect.service.spec.ts
+++ src/app/services/walletconnect/walletconnect.service.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from '@angular/core/testing'
 
+import { UnitHelper } from '../../../../test-config/unit-test-helper'
+
 import { WalletconnectService } from './walletconnect.service'
 
 describe('WalletconnectService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}))
+  let unitHelper: UnitHelper
+  beforeEach(() => {
+    unitHelper = new UnitHelper()
+
+    TestBed.configureTestingModule(
+      unitHelper.testBed({
+        providers: []
+      })
+    )
+      .compileComponents()
+      .catch(console.error)
+  })
 
   it('should be created', () => {
     const service: WalletconnectService = TestBed.get(WalletconnectService)
