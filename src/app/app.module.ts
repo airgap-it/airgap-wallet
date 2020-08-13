09f6b023759e521b7179701a268b178dffffc640
Julia Samól <j.samol@papers.ch>
2020-08-13 12:22:54 +0200

    chore(): add common components library (@airgap/angular-core)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AirGapCommonCoreModule, AirGapCommonTranslateLoader } from '@airgap/angular-core'
 import { CommonModule, DecimalPipe } from '@angular/common'
 import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
@@ -13,44 +14,14 @@ import { QRScanner } from '@ionic-native/qr-scanner/ngx'
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular'
 import { IonicStorageModule } from '@ionic/storage'
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
-import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { ZXingScannerModule } from '@zxing/ngx-scanner'
+import { MaterialIconsModule } from 'ionic2-material-icons'
 import { ChartsModule } from 'ng2-charts'
+import { LottieModule } from 'ngx-lottie'
 import { MomentModule } from 'ngx-moment'
-import { ExchangeSelectPageModule } from './pages/exchange-select/exchange-select.module'
-import { ExchangeSelectPage } from './pages/exchange-select/exchange-select.page'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { ComponentsModule } from './components/components.module'
-import { IntroductionPushPage } from './pages/introduction-push/introduction-push'
-import { IntroductionPushPageModule } from './pages/introduction-push/introduction-push.module'
-import { ProtocolSelectPage } from './pages/protocol-select/protocol-select'
-import { ProtocolSelectPageModule } from './pages/protocol-select/protocol-select.module'
-import { AmountConverterPipe } from './pipes/amount-converter/amount-converter.pipe'
-import { PipesModule } from './pipes/pipes.module'
-import { AccountProvider } from './services/account/account.provider'
-import { AppInfoProvider } from './services/app-info/app-info'
-import { ClipboardService } from './services/clipboard/clipboard'
-import { DeepLinkProvider } from './services/deep-link/deep-link'
-import { DrawChartService } from './services/draw-chart/draw-chart.service'
-import { ExchangeProvider } from './services/exchange/exchange'
-import { MarketDataService } from './services/market-data/market-data.service'
-import { OperationsProvider } from './services/operations/operations'
-import { PermissionsProvider } from './services/permissions/permissions'
-
-import { BeaconRequestPageModule } from './pages/beacon-request/beacon-request.module'
-import { BeaconRequestPage } from './pages/beacon-request/beacon-request.page'
-import { ShortenStringPipe } from './pipes/shorten-string/shorten-string.pipe'
-import { ExtensionsService } from './services/extensions/extensions.service'
-import { PushBackendProvider } from './services/push-backend/push-backend'
-import { PushProvider } from './services/push/push'
-import { RemoteConfigProvider } from './services/remote-config/remote-config'
-import { ScannerProvider } from './services/scanner/scanner'
-import { SchemeRoutingProvider } from './services/scheme-routing/scheme-routing'
-import { SerializerService } from './services/serializer/serializer.service'
-import { StorageProvider } from './services/storage/storage'
-
 import {
   APP_INFO_PLUGIN,
   APP_PLUGIN,
@@ -62,12 +33,42 @@ import {
   SPLASH_SCREEN_PLUGIN,
   STATUS_BAR_PLUGIN
 } from './capacitor-plugins/injection-tokens'
+import { ComponentsModule } from './components/components.module'
+import { BeaconRequestPageModule } from './pages/beacon-request/beacon-request.module'
+import { BeaconRequestPage } from './pages/beacon-request/beacon-request.page'
+import { ExchangeSelectPageModule } from './pages/exchange-select/exchange-select.module'
+import { ExchangeSelectPage } from './pages/exchange-select/exchange-select.page'
+import { IntroductionPushPage } from './pages/introduction-push/introduction-push'
+import { IntroductionPushPageModule } from './pages/introduction-push/introduction-push.module'
+import { ProtocolSelectPage } from './pages/protocol-select/protocol-select'
+import { ProtocolSelectPageModule } from './pages/protocol-select/protocol-select.module'
+import { PipesModule } from './pipes/pipes.module'
+import { ShortenStringPipe } from './pipes/shorten-string/shorten-string.pipe'
+import { AccountProvider } from './services/account/account.provider'
+import { AppInfoProvider } from './services/app-info/app-info'
+import { ClipboardService } from './services/clipboard/clipboard'
+import { DeepLinkProvider } from './services/deep-link/deep-link'
+import { DrawChartService } from './services/draw-chart/draw-chart.service'
+import { ExchangeProvider } from './services/exchange/exchange'
+import { ExtensionsService } from './services/extensions/extensions.service'
+import { MarketDataService } from './services/market-data/market-data.service'
+import { OperationsProvider } from './services/operations/operations'
+import { PermissionsProvider } from './services/permissions/permissions'
 import { ProtocolsProvider } from './services/protocols/protocols'
+import { PushBackendProvider } from './services/push-backend/push-backend'
+import { PushProvider } from './services/push/push'
+import { RemoteConfigProvider } from './services/remote-config/remote-config'
+import { ScannerProvider } from './services/scanner/scanner'
+import { SchemeRoutingProvider } from './services/scheme-routing/scheme-routing'
+import { SerializerService } from './services/serializer/serializer.service'
+import { StorageProvider } from './services/storage/storage'
 
-const { App, AppInfo, Browser, Clipboard, Permissions, PushNotifications, Share, SplashScreen, StatusBar } = Plugins
+export function createTranslateLoader(http: HttpClient): AirGapCommonTranslateLoader {
+  return new AirGapCommonTranslateLoader(http, { prefix: './assets/i18n/', suffix: '.json' })
+}
 
-export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json')
+export async function createPlayer() {
+  return import('lottie-web')
 }
 
 @NgModule({
@@ -81,12 +82,14 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
     ReactiveFormsModule,
     FormsModule,
     CommonModule,
+    AirGapCommonCoreModule,
     ZXingScannerModule,
-    HttpClientModule,
-
+    MaterialIconsModule,
     MomentModule,
+    LottieModule.forRoot({ player: createPlayer }),
     IonicModule.forRoot(),
     AppRoutingModule,
+    HttpClientModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -106,19 +109,18 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
     IntroductionPushPageModule
   ],
   providers: [
-    { provide: APP_PLUGIN, useValue: App },
-    { provide: APP_INFO_PLUGIN, useValue: AppInfo },
-    { provide: BROWSER_PLUGIN, useValue: Browser },
-    { provide: CLIPBOARD_PLUGIN, useValue: Clipboard },
-    { provide: PERMISSIONS_PLUGIN, useValue: Permissions },
-    { provide: PUSH_NOTIFICATIONS_PLUGIN, useValue: PushNotifications },
-    { provide: SHARE_PLUGIN, useValue: Share },
-    { provide: SPLASH_SCREEN_PLUGIN, useValue: SplashScreen },
-    { provide: STATUS_BAR_PLUGIN, useValue: StatusBar },
+    { provide: APP_PLUGIN, useValue: Plugins.App },
+    { provide: APP_INFO_PLUGIN, useValue: Plugins.AppInfo },
+    { provide: BROWSER_PLUGIN, useValue: Plugins.Browser },
+    { provide: CLIPBOARD_PLUGIN, useValue: Plugins.Clipboard },
+    { provide: PERMISSIONS_PLUGIN, useValue: Plugins.Permissions },
+    { provide: PUSH_NOTIFICATIONS_PLUGIN, useValue: Plugins.PushNotifications },
+    { provide: SHARE_PLUGIN, useValue: Plugins.Share },
+    { provide: SPLASH_SCREEN_PLUGIN, useValue: Plugins.SplashScreen },
+    { provide: STATUS_BAR_PLUGIN, useValue: Plugins.StatusBar },
     BarcodeScanner,
     QRScanner,
     Keyboard,
-    AmountConverterPipe,
     DecimalPipe,
     ShortenStringPipe,
     MarketDataService,
