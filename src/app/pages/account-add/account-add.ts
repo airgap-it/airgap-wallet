108faa55de248b73c7451b1b102a86cbf511492d
acharl <amadeo@fermat.org>
2020-09-18 16:49:25 +0200

    feat(): distinguish between featured and other subaccounts

--- src/app/pages/account-add/account-add.ts
+++ src/app/pages/account-add/account-add.ts
@@ -1,13 +1,20 @@
+import { MainProtocolSymbols } from 'airgap-coin-lib/dist/utils/ProtocolSymbols'
 import { ProtocolService } from '@airgap/angular-core'
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
-import { ICoinProtocol, supportedProtocols } from 'airgap-coin-lib'
-import { SubProtocolType } from 'airgap-coin-lib/dist/protocols/ICoinSubProtocol'
-import { NetworkType } from 'airgap-coin-lib/dist/utils/ProtocolNetwork'
+import { ICoinProtocol } from 'airgap-coin-lib'
 
 import { AccountProvider } from '../../services/account/account.provider'
 import { DataService, DataServiceKey } from '../../services/data/data.service'
 import { ErrorCategory, handleErrorSentry } from '../../services/sentry-error-handler/sentry-error-handler'
+
+export enum FeaturedSubProtocolSymbols {
+  XTZ_KT = 'xtz-kt',
+  XTZ_BTC = 'xtz-btc',
+  XTZ_USD = 'xtz-usd',
+  XTZ_STKR = 'xtz-stkr',
+  XCHF = 'eth-erc20-xchf'
+}
 
 @Component({
   selector: 'page-account-add',
@@ -17,35 +24,32 @@ import { ErrorCategory, handleErrorSentry } from '../../services/sentry-error-ha
 export class AccountAddPage {
   public searchTerm: string = ''
   public supportedAccountProtocols: ICoinProtocol[] = []
-  public supportedSubAccountProtocols: ICoinProtocol[] = []
+  public featuredSubAccountProtocols: ICoinProtocol[] = []
+  public otherSubAccountProtocols: ICoinProtocol[] = []
   public filteredAccountProtocols: ICoinProtocol[] = []
-  public filteredSubAccountProtocols: ICoinProtocol[] = []
+  public filteredFeaturedSubAccountProtocols: ICoinProtocol[] = []
+  public filteredOtherSubAccountProtocols: ICoinProtocol[] = []
 
   constructor(
     private readonly accountProvider: AccountProvider,
     private readonly protocolService: ProtocolService,
     private readonly router: Router,
     private readonly dataService: DataService
-  ) {
-    this.supportedAccountProtocols = supportedProtocols()
-      .filter((protocol: ICoinProtocol) => protocol.options.network.type === NetworkType.MAINNET)
-      .map(coin => coin)
-    console.log('supportedProtocols()', supportedProtocols())
-    this.supportedSubAccountProtocols = supportedProtocols()
-      .filter((protocol: ICoinProtocol) => protocol.options.network.type === NetworkType.MAINNET)
-      .reduce((pv, cv) => {
-        if (cv.subProtocols) {
-          const subProtocols = cv.subProtocols.filter(
-            subProtocol => subProtocol.subProtocolType === SubProtocolType.TOKEN && this.protocolService.isProtocolActive(subProtocol)
-          )
+  ) {}
 
-          return pv.concat(...subProtocols)
-        }
+  public async ionViewWillEnter() {
+    this.supportedAccountProtocols = await this.protocolService.getActiveProtocols()
+    const supportedSubAccountProtocols = Array.prototype.concat.apply(
+      [],
+      await Promise.all(Object.values(MainProtocolSymbols).map(protocol => this.protocolService.getSubProtocols(protocol)))
+    )
 
-        return pv
-      }, [])
-    console.log('supportedSubAccountProtocols', this.supportedSubAccountProtocols)
-
+    this.featuredSubAccountProtocols = supportedSubAccountProtocols.filter(protocol =>
+      Object.values(FeaturedSubProtocolSymbols).includes(protocol.identifier.toLowerCase() as FeaturedSubProtocolSymbols)
+    )
+    this.otherSubAccountProtocols = supportedSubAccountProtocols.filter(
+      protocol => !Object.values(FeaturedSubProtocolSymbols).includes(protocol.identifier.toLowerCase() as FeaturedSubProtocolSymbols)
+    )
     this.filterProtocols()
   }
 
@@ -55,10 +59,14 @@ export class AccountAddPage {
 
   public filterProtocols() {
     const lowerCaseSearchTerm = this.searchTerm.toLowerCase()
+
     this.filteredAccountProtocols = this.supportedAccountProtocols.filter(
       protocol => protocol.name.toLowerCase().includes(lowerCaseSearchTerm) || protocol.symbol.toLowerCase().includes(lowerCaseSearchTerm)
     )
-    this.filteredSubAccountProtocols = this.supportedSubAccountProtocols.filter(
+    this.filteredFeaturedSubAccountProtocols = this.featuredSubAccountProtocols.filter(
+      protocol => protocol.name.toLowerCase().includes(lowerCaseSearchTerm) || protocol.symbol.toLowerCase().includes(lowerCaseSearchTerm)
+    )
+    this.filteredOtherSubAccountProtocols = this.otherSubAccountProtocols.filter(
       protocol => protocol.name.toLowerCase().includes(lowerCaseSearchTerm) || protocol.symbol.toLowerCase().includes(lowerCaseSearchTerm)
     )
   }
@@ -88,7 +96,7 @@ export class AccountAddPage {
       }
 
       this.dataService.setData(DataServiceKey.PROTOCOL, info)
-      this.router.navigateByUrl('/sub-account-import/' + DataServiceKey.PROTOCOL).catch(handleErrorSentry(ErrorCategory.NAVIGATION))
+      this.router.navigateByUrl('/sub-account-import/' + DataServiceKey.PROTOCOL).catch(err => console.error(err))
     } else {
       const info = {
         mainProtocolIdentifier: mainProtocolIdentifier,
