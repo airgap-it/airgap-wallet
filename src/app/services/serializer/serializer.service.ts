71e66a882d9613c9c8b55090627accbbf71bf760
Julia Samól <j.samol@papers.ch>
2020-09-23 17:23:52 +0200

    feat(serializer): enable v2 by default

--- src/app/services/serializer/serializer.service.ts
+++ src/app/services/serializer/serializer.service.ts
@@ -16,7 +16,7 @@ export class SerializerService {
   private readonly v1Tov2Mapping: Map<number, number> = new Map<number, number>()
   private readonly v2Tov1Mapping: Map<number, number> = new Map<number, number>()
 
-  private _useV2: boolean = false
+  private _useV2: boolean = true
   private _chunkSize: number = 100
   private _displayTimePerChunk: number = 500
 
