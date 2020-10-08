8db1e671ab9a04dafe9010bd6ee9cd9f93e6a324
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-10-08 16:57:33 +0000

    feat(beacon): add base58 support to beacon handler

--- src/app/services/iac/custom-handlers/beacon-handler.ts
+++ src/app/services/iac/custom-handlers/beacon-handler.ts
@@ -1,13 +1,14 @@
 import { IACMessageHandler } from '@airgap/angular-core'
-import { P2PPairInfo } from '@airgap/beacon-sdk'
+import { P2PPairingRequest, Serializer } from '@airgap/beacon-sdk'
 
 import { BeaconService } from '../../beacon/beacon.service'
 
-const isBeaconMessage: (obj: unknown) => obj is P2PPairInfo = (obj: unknown): obj is P2PPairInfo => {
+const isBeaconMessage: (obj: unknown) => obj is P2PPairingRequest = (obj: unknown): obj is P2PPairingRequest => {
   return (
-    typeof (obj as P2PPairInfo).name === 'string' &&
-    typeof (obj as P2PPairInfo).publicKey === 'string' &&
-    typeof (obj as P2PPairInfo).relayServer === 'string'
+    typeof (obj as P2PPairingRequest).name === 'string' &&
+    typeof (obj as P2PPairingRequest).publicKey === 'string' &&
+    typeof (obj as P2PPairingRequest).relayServer === 'string'
+    // version is not checked to be v1 compatible
   )
 }
 
@@ -24,19 +25,41 @@ export class BeaconHandler extends IACMessageHandler {
   }
 
   public async handle(data: string | string[]): Promise<boolean> {
-    let json: Record<string, unknown> | undefined
-    try {
-      json = JSON.parse(typeof data === 'string' ? data : data[0])
-    } catch (jsonParseError) {
-      return false
+    const tryHandleBeacon: (json: unknown) => Promise<void> = async (json: unknown): Promise<void> => {
+      if (isBeaconMessage(json)) {
+        console.log('Beacon Pairing QR scanned', json)
+
+        await this.beaconService.addPeer({
+          name: json.name,
+          publicKey: json.publicKey,
+          relayServer: json.relayServer,
+          version: json.version
+        })
+      }
     }
 
-    if (isBeaconMessage(json)) {
-      console.log('Beacon Pairing QR scanned', json)
-
-      await this.beaconService.addPeer({ name: json.name, publicKey: json.publicKey, relayServer: json.relayServer })
+    // Check if it's a beacon request
+    try {
+      const json: Record<string, unknown> = JSON.parse(typeof data === 'string' ? data : data[0])
+      await tryHandleBeacon(json)
 
       return true
+    } catch (e) {
+      try {
+        const payload: string = typeof data === 'string' ? data : data[0]
+        if (payload.includes('tezos://')) {
+          const params: URLSearchParams = new URL(payload).searchParams
+          if (params && params.get('type') === 'tzip10') {
+            const json: Record<string, unknown> = (await new Serializer().deserialize(params.get('data'))) as any
+            await tryHandleBeacon(json)
+
+            return true
+          }
+        }
+      } catch (err) {
+        //
+      }
+      //
     }
 
     return false
