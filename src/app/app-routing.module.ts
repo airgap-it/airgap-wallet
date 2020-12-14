870ca4cc96200c0ba9873eee605267ead5a9a119
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-12-14 20:29:58 +0000

    feat(angular): update to version 11 and latest ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -189,7 +189,7 @@ const routes: Routes = [
   }
 ]
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'corrected' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
