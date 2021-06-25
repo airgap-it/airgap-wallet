bef000b36a67cad0f2e3d34598020c93ff579251
acharl <amadeo.charle108@gmail.com>
2021-06-25 12:47:35 +0200

    chore(): remove deprecated FirebaseInstanceID in AppDelegate.swift

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -65,7 +65,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
     NotificationCenter.default.post(name: Notification.Name(CAPNotifications.DidRegisterForRemoteNotificationsWithDeviceToken.name()), object: deviceToken)
     Messaging.messaging().apnsToken = deviceToken
-    InstanceID.instanceID().instanceID { (result, error) in
+    Messaging.messaging().token { (token, error) in
         if let error = error {
             NotificationCenter.default.post(name: Notification.Name(CAPNotifications.DidFailToRegisterForRemoteNotificationsWithError.name()), object: error)
         } else if let result = result {
