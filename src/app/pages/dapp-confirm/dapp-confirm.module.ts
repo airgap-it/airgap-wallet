10663b4c47479fe2f385bd7a6352c5138c935ed5
acharl <amadeo.charle108@gmail.com>
2021-07-02 15:16:45 +0200

    fix(): add missing module import for DappConfirmPageModule

--- src/app/pages/dapp-confirm/dapp-confirm.module.ts
+++ src/app/pages/dapp-confirm/dapp-confirm.module.ts
@@ -1,20 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular'
 
-import { DappConfirmPageRoutingModule } from './dapp-confirm-routing.module';
+import { DappConfirmPageRoutingModule } from './dapp-confirm-routing.module'
 
-import { DappConfirmPage } from './dapp-confirm.page';
+import { DappConfirmPage } from './dapp-confirm.page'
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    DappConfirmPageRoutingModule
-  ],
+  imports: [CommonModule, FormsModule, IonicModule, TranslateModule, DappConfirmPageRoutingModule],
   declarations: [DappConfirmPage]
 })
 export class DappConfirmPageModule {}
