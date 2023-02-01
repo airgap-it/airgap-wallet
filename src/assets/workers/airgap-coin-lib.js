68fd98e1b02ca3b81a6457179a183d33f5024101
Mike Godenzi <m.godenzi@papers.ch>
2023-02-01 15:40:27 +0100

    Feat/icp integration

--- src/assets/workers/airgap-coin-lib.js
+++ src/assets/workers/airgap-coin-lib.js
@@ -1,16 +1,17 @@
-var window = self;
+var window = self
 
 // available as airgapCoinLib due to browserify postInstall hook
-self.importScripts("../libs/airgap-coin-lib.browserify.js");
-self.importScripts("../libs/airgap-aeternity.browserify.js");
-self.importScripts("../libs/airgap-astar.browserify.js");
-self.importScripts("../libs/airgap-bitcoin.browserify.js");
-self.importScripts("../libs/airgap-cosmos.browserify.js");
-self.importScripts("../libs/airgap-ethereum.browserify.js");
-self.importScripts("../libs/airgap-groestlcoin.browserify.js");
-self.importScripts("../libs/airgap-moonbeam.browserify.js");
-self.importScripts("../libs/airgap-polkadot.browserify.js");
-self.importScripts("../libs/airgap-tezos.browserify.js");
+self.importScripts('../libs/airgap-coin-lib.browserify.js')
+self.importScripts('../libs/airgap-aeternity.browserify.js')
+self.importScripts('../libs/airgap-astar.browserify.js')
+self.importScripts('../libs/airgap-bitcoin.browserify.js')
+self.importScripts('../libs/airgap-cosmos.browserify.js')
+self.importScripts('../libs/airgap-ethereum.browserify.js')
+self.importScripts('../libs/airgap-groestlcoin.browserify.js')
+self.importScripts('../libs/airgap-moonbeam.browserify.js')
+self.importScripts('../libs/airgap-polkadot.browserify.js')
+self.importScripts('../libs/airgap-tezos.browserify.js')
+self.importScripts('../libs/airgap-icp.browserify.js')
 
 const protocols = [
   new airgapCoinLibAeternity.AeternityProtocol(),
@@ -29,31 +30,45 @@ const protocols = [
   new airgapCoinLibTezos.TezosShieldedTezProtocol()
 ]
 
-const getProtocolByIdentifier = (identifier) => {
+const getProtocolByIdentifier = async (identifier) => {
+  if (identifier === 'icp') {
+    const module = new airgapCoinLibICP.ICPModule()
+    let protocol = await module.createOnlineProtocol('icp')
+    protocol.getAddressesFromPublicKey = async (publicKey, cursor) => {
+      const address = await protocol.getAddressFromPublicKey({ value: publicKey })
+      return [{ address: address }]
+    }
+    return protocol
+  }
   return protocols.find((protocol) => protocol.identifier === identifier)
 }
 
-self.onmessage = event => {
-  const wallets = event.data;
+self.onmessage = (event) => {
+  const wallets = event.data
 
-  Promise.all(wallets.map(wallet => {
-    const protocol = getProtocolByIdentifier(wallet.protocolIdentifier)
+  Promise.all(
+    wallets.map(async (wallet) => {
+      let protocol = await getProtocolByIdentifier(wallet.protocolIdentifier)
 
-    const airGapWallet = new airgapCoinLib.AirGapWallet(
-      protocol,
-      wallet.publicKey,
-      wallet.isExtendedPublicKey,
-      wallet.derivationPath,
-      wallet.masterFingerprint,
-      wallet.status
-    );
-    return airGapWallet.deriveAddresses(50).then(addresses => {
-      return {addresses: addresses, key: `${wallet.protocolIdentifier}_${wallet.publicKey}`}
+      const airGapWallet = new airgapCoinLib.AirGapWallet(
+        protocol,
+        wallet.publicKey,
+        wallet.isExtendedPublicKey,
+        wallet.derivationPath,
+        wallet.masterFingerprint,
+        wallet.status
+      )
+
+      return airGapWallet.deriveAddresses(50).then((addresses) => {
+        return { addresses: addresses, key: `${wallet.protocolIdentifier}_${wallet.publicKey}` }
+      })
     })
-  })).then(async (addressesByKeys) => {
-    const derivedAddressesMap = addressesByKeys.reduce((obj, addressesByKey) => Object.assign(obj, { [addressesByKey.key]: addressesByKey.addresses }), {})
-    self.postMessage(derivedAddressesMap);
+  ).then(async (addressesByKeys) => {
+    const derivedAddressesMap = addressesByKeys.reduce(
+      (obj, addressesByKey) => Object.assign(obj, { [addressesByKey.key]: addressesByKey.addresses }),
+      {}
+    )
+
+    self.postMessage(derivedAddressesMap)
   })
 }
-
-               
