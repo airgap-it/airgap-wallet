9336a87f08db34edea2616218b1b63314c02a2ca
Stephen Debel <s.debel@papers.ch>
2024-02-12 13:37:18 +0100

    Revert "Resolve "Remove Mt. Pelerin functionality""

--- src/app/pages/account-transaction-list/account-transaction-list.ts
+++ src/app/pages/account-transaction-list/account-transaction-list.ts
@@ -1,4 +1,10 @@
-import { createV0TezosKtProtocol, ICoinSubProtocolAdapter, ProtocolService } from '@airgap/angular-core'
+import {
+  createV0TezosKtProtocol,
+  ICoinSubProtocolAdapter,
+  InternalStorageKey,
+  InternalStorageService,
+  ProtocolService
+} from '@airgap/angular-core'
 import { AirGapMarketWallet, IAirGapTransaction, MainProtocolSymbols, SubProtocolSymbols } from '@airgap/coinlib-core'
 import { Action } from '@airgap/coinlib-core/actions/Action'
 import { IAirGapAddressResult } from '@airgap/coinlib-core/interfaces/IAirGapAddress'
@@ -116,6 +122,7 @@ export class AccountTransactionListPage {
     private readonly pushBackendProvider: PushBackendProvider,
     private readonly extensionsService: ExtensionsService,
     private readonly browserService: BrowserService,
+    private readonly storageService: InternalStorageService,
     private readonly modalController: ModalController
   ) {
     this.isDesktop = this.platform.is('desktop')
@@ -158,6 +165,18 @@ export class AccountTransactionListPage {
     this.actionGroup = new ActionGroup(this)
     this.actionGroup.getActions().then((actions) => {
       this.actions = actions
+    })
+
+    // Mt Perelin
+    this.storageService.get(InternalStorageKey.SETTINGS_TRADING_USE_MTPELERIN).then((active) => {
+      if (active) {
+        this.storageProvider.getCache('mtperelin-currencies').then((savedCurrencies) => {
+          this.wallet.protocol.getSymbol().then((symbol) => {
+            const validCurrency = Object.values(savedCurrencies).find((currency) => currency.symbol === symbol)
+            this.isMtPerelinActive = !!active && !!validCurrency
+          })
+        })
+      }
     })
   }
 
@@ -414,6 +433,22 @@ export class AccountTransactionListPage {
       modal.onDidDismiss().then(() => {
         resolve()
       })
+    })
+  }
+
+  public async buyMtPerelin() {
+    this.wallet.protocol.getSymbol().then(async (symbol) => {
+      const url = `https://buy.mtpelerin.com/?type=direct-link&bdc=${symbol}&rfr=bcH4RmHm&_ctkn=954139b2-ef3e-4914-82ea-33192d3f43d3`
+      await this.openModal(url)
+      // window.open(`https://buy.mtpelerin.com/?type=direct-link&bdc=${symbol}&rfr=bcH4RmHm`, '_blank')
+    })
+  }
+
+  public async sellMtPerelin() {
+    this.wallet.protocol.getSymbol().then(async (symbol) => {
+      const url = `https://sell.mtpelerin.com/?type=direct-link&tab=sell&ssc=${symbol}&rfr=bcH4RmHm&_ctkn=954139b2-ef3e-4914-82ea-33192d3f43d3`
+      await this.openModal(url)
+      // window.open(`https://sell.mtpelerin.com/?type=direct-link&tab=sell&ssc=${symbol}&rfr=bcH4RmHm`, '_blank')
     })
   }
 }
