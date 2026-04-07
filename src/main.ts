c9e2dc104461d2dea938ecdc157db0294b94ae99
Stephen Debel <s.debel@papers.ch>
2026-04-07 11:18:54 +0000

    Chores/sentry

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import '@angular/compiler'
 
 import { enableProdMode } from '@angular/core'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import * as Sentry from '@sentry/capacitor'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as SentryAngular from '@sentry/angular'
 
 import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
+
+Sentry.init(
+  {
+    dsn: 'https://20fcaea4b311d1e1d7a2abfc7d246b56@watcher.papers.tech/9',
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 1.0
+  },
+  SentryAngular.init
+)
 
 if (environment.production) {
   enableProdMode()
